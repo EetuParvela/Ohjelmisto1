0c48c32afac54f48ee781df9db6a71ec5c27ea4b
EetuParvela <Eetu.Parvela@metropolia.fi>
2025-01-14 23:27:16 +0200

    Lisätty tehtävät 2.1, 2.2 ja 2.3

---
+++ Moduuli1/Tehtävä 2.2.py
@@ -0,0 +1,5 @@
+import math
+
+radius = float(input('Mikä on ympyrän säde?: '))
+area = math.pi * float(radius) ** 2
+print(f'Ympyrän pinta-ala on : {area}')
---
+++ Moduuli1/Tehtävä 2.3.py
@@ -0,0 +1,5 @@
+base, height = (float(input('Mikä on suorakulmion kanta?: ')), float(input('Mikä on suorakulmion korkeus?: ')))
+area = base * height
+perimeter = 2 * (base + height)
+
+print(f'Suorakulmion pinta-ala on {area} ja piiri on {perimeter}')
